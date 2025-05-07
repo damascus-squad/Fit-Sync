a702b022d4e4a43c021642d761ccf09b884ebc55
AbdulrahmanMagdey <abdomagdey5@gmail.com>
2025-05-07 14:30:32 +0300

    separate weather data model

--- src/main/kotlin/domain/model/WeatherInfo.kt
+++ src/main/kotlin/domain/model/WeatherInfo.kt
@@ -1,20 +1,5 @@
 package org.damascus.domain.model
 
-data class Weather(
-    val temperature: Double,
-    val windSpeed: Double,
-    val windDirection: Int,
-    val isDay: Boolean,
-    val weatherCode: Int,
-    val time: String
-)
-
-data class WeatherUnit(
-    val temperatureUnit: String,
-    val windSpeedUnit: String,
-    val windDirectionUnit: String
-)
-
 data class WeatherInfo(
     val latitude: Double,
     val longitude: Double,
