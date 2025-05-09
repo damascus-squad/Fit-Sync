ff37f5ff159a1142a1f5a40018ee551d24013b62
Mohammed Ashour <83330641+mohammedyashour@users.noreply.github.com>
2025-05-10 01:29:47 +0300

    Update ClothesRepository.kt

--- src/main/kotlin/domain/repository/ClothesRepository.kt
+++ src/main/kotlin/domain/repository/ClothesRepository.kt
@@ -4,6 +4,6 @@ import org.damascus.domain.model.Cloth
 import org.damascus.domain.model.ClothType
 
 interface ClothesRepository {
-    fun getClothsByType(clothType: ClothType): List<Cloth>
+    fun getClothesByType(clothType: ClothType): List<Cloth>
     fun getAllClothes(): List<Cloth>
 }
