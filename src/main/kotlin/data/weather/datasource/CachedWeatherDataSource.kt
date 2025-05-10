2eeb18cd0267f896df6d074765330cb51f5e9bdb
Mohamed Boumaraf <boumarafmohamed.co@gmail.com>
2025-05-10 15:25:32 +0300

    refactor CachedWeatherDataSource.kt by making getWeather() into two methods

--- src/main/kotlin/data/weather/datasource/CachedWeatherDataSource.kt
+++ src/main/kotlin/data/weather/datasource/CachedWeatherDataSource.kt
@@ -1,5 +1,6 @@
 package org.damascus.data.weather.datasource
 
+import org.damascus.data.location.dataSource.LocationDataSource
 import org.damascus.data.weather.cache.io.WeatherCacheService
 import org.damascus.data.weather.mapper.WeatherDataConverter
 import org.damascus.data.weather.dto.WeatherDto
@@ -10,15 +11,28 @@ class CachedWeatherDataSource(
     private val apiClient: WeatherDataSource,
     private val cacheService: WeatherCacheService,
     private val converter: WeatherDataConverter,
+    private val locationDataSource: LocationDataSource,
     private val cacheDurationMinutes: Long = 30
-) {
+) : WeatherDataSource {
 
-    suspend fun getWeather(locationCoordinate: LocationCoordinate): WeatherDto {
+    override suspend fun getWeatherByCity(cityName: String, country: String): WeatherDto {
+        val location = locationDataSource.getCityCoordinates(cityName, country)
+            ?: throw LocationNotFoundException(cityName, country)
+        return getCachedWeather(LocationCoordinate(location.latitude, location.longitude))
+    }
+
+    override suspend fun getWeatherByIp(): WeatherDto {
+        val location = locationDataSource.getCurrentLocation()
+            ?: throw LocationNotFoundExceptionIp()
+        return getCachedWeather(LocationCoordinate(location.latitude, location.longitude))
+    }
+
+    private suspend fun getCachedWeather(locationCoordinate: LocationCoordinate): WeatherDto {
         val cachedWeatherInfo = cacheService.getValidFromCache(locationCoordinate, cacheDurationMinutes)
         if (cachedWeatherInfo != null)
             return converter.weatherInfoToDto(cachedWeatherInfo)
 
-        val weatherDtoFromApi = apiClient.getWeather(locationCoordinate)
+        val weatherDtoFromApi = apiClient.getWeatherByCity(locationCoordinate.latitude.toString(), locationCoordinate.longitude.toString())
         val weatherInfoToCache: WeatherInfo = converter.dtoToWeatherInfo(weatherDtoFromApi)
         cacheService.saveToCache(weatherInfoToCache)
         return weatherDtoFromApi
