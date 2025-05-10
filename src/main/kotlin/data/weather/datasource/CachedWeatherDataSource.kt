6728517f2ba3965eecc937c63f3d432c7a1d4d43
Mohamed Boumaraf <boumarafmohamed.co@gmail.com>
2025-05-09 22:05:20 -0700

    improve csvCashing use case

--- src/main/kotlin/data/weather/datasource/CachedWeatherDataSource.kt
+++ src/main/kotlin/data/weather/datasource/CachedWeatherDataSource.kt
@@ -1,6 +1,7 @@
 package org.damascus.data.weather.datasource
 
-import org.damascus.data.weather.mapper.WeatherCacheEntryConverter
+import org.damascus.data.weather.cache.io.WeatherCacheService
+import org.damascus.data.weather.mapper.WeatherDataConverter
 import org.damascus.data.weather.dto.WeatherDto
 import org.damascus.domain.model.LocationCoordinate
 import org.damascus.domain.model.WeatherInfo
@@ -8,11 +9,11 @@ import org.damascus.domain.model.WeatherInfo
 class CachedWeatherDataSource(
     private val apiClient: WeatherDataSource,
     private val cacheService: WeatherCacheService,
-    private val converter: WeatherCacheEntryConverter,
+    private val converter: WeatherDataConverter,
     private val cacheDurationMinutes: Long = 30
-) : WeatherDataSource {
+) {
 
-    override suspend fun getWeather(locationCoordinate: LocationCoordinate): WeatherDto {
+    suspend fun getWeather(locationCoordinate: LocationCoordinate): WeatherDto {
         val cachedWeatherInfo = cacheService.getValidFromCache(locationCoordinate, cacheDurationMinutes)
         if (cachedWeatherInfo != null)
             return converter.weatherInfoToDto(cachedWeatherInfo)
